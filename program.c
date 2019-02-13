2c4d9237325c7f3e41520f51e1e2dc0d54a801c6
MaxxReis <marcelo.dalta@gmail.com>
2019-02-13 17:24:26 -0300

    Implementando criar conta

--- program.c
+++ program.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 typedef struct Account{
     int id;
-    char* name;
+    char *name;
     double balance;
 } Account;
 
 int numAccounts = 0;
-char** names = 
+char names[3][8] = {"Fulano", "Cicrano", "Beltrano"};
 
 Account *accounts;
 
@@ -16,21 +17,54 @@ void inicializeBank(){
     accounts = (struct Account*) malloc(sizeof(struct Account));
 }
 
-void addAccount(struct Account account){
-    accounts[numAccounts].id = account.id;
-    accounts[numAccounts].name = account.name;
-    accounts[numAccounts].balance = account.balance;
-    numAccounts++;
-    
+Account* findAccountById(int id){
+    for(int i=0; i<numAccounts;i++)
+        if(accounts[i].id == id)
+            return &accounts[i];
+    return NULL;
 }
 
-Account createAccount(){
-    Account newAccount;
+Account* createAccount(int id){
+    srand(time(NULL));
+    Account *newAccount = (Account *) malloc(sizeof(Account));;
+    newAccount->id = id;
+    newAccount->name = names[rand()%3];
+    newAccount->balance = 0.0;
+    return newAccount;
+}
+
+int addAccount(Account *account){
+        accounts[numAccounts].id = account->id;
+        accounts[numAccounts].name = account->name;
+        accounts[numAccounts].balance = account->balance;
+        printf("\nId: %d",accounts[numAccounts].id);
+        printf("\nName: %s",accounts[numAccounts].name);
+        printf("\nSaldo: %f",accounts[numAccounts].balance);
+        printf("\n");
+        return account->id;
+}
+
+int changeAccount(Account *account, double value){
+    if((account->balance + value) < 0.0)
+        return 0;
+    account->balance += value;
+    return 1;
+}
+
+int getIdFromUser(){
+    int id = 0;
     printf("Digite o id da conta: ");
-    scanf("%d", &newAccount.id);
-    
-    
-    
+    scanf("%d", &id);
+    printf("\n");
+    return id;
+}
+
+double getValueFromUser(){
+    double value = 0;
+    printf("Digite o valor: ");
+    scanf("%lf", &value);
+    printf("\n");
+    return value;
 }
 
 void listAccounts(){
@@ -41,19 +75,45 @@ void listAccounts(){
         printf("\n");
     }
 }
-//void imprimirInt(int);
+
+void showMenu(){
+    printf("Selecione a opcao desejada:\n");
+    printf("\t1 - Criar Conta\n");
+    printf("\t2 - Depositar em Conta\n");
+    printf("\t3 - Sacar\n");
+    printf("\t4 - Ver Saldo\n");
+}
+
+void testPercorrerVetor(){
+    for (int i = 0; i<50; i++){
+        printf("Criando conta: %d\n", addAccount(createAccount(i)));
+        srand(time(NULL));
+    }
+    listAccounts();
+}
 
 int main(){
     inicializeBank();
+    testPercorrerVetor();/*
+    showMenu();
+    do{
+        int opc =0;
+        scanf("%d", &opc);
+    switch(opc){
+        case 0:
+            printf("Encerrando a aplicacao\n");
+            return 0;
+        case 1:
+            addAccount(createAccount(getIdFromUser()));
+            break;
+        case 2:
+            break;
+        
+    }
+    }while(opc);
     
-    /*struct Account myAccount;
-    myAccount.id = 1;
-    myAccount.name = "Joao Silva";
-    myAccount.balance = 50;
-    */
-    
-    createAccount();
-    listAccounts();
+    //createAccount();
+    listAccounts();*/
     
 }
 
